37a62f5fea0265607f92acdbd1b227539c3ccc38
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-26 14:20:51 +0530

    Adding more test.

--- test/libTest.js
+++ test/libTest.js
@@ -8,13 +8,27 @@ const mockReader = function (expectedFiles) {
 };
 
 describe("contentCount", () => {
+    let files = {};
+    let fs = {};
+    beforeEach(() => {
+        files.alphabets = "a\nb\nc\nd\ne";
+        files.numbers = "1\nb\nc\nd\ne";
+        files.empty = "";
+        fs.readFileSync = mockReader(files);
+    })
     it('should return number of lines, words and chars with filename', () => {
-        const file = { alphabets:"a\nb\nc\nd\ne" };
-        let fs = { readFileSync: mockReader(file)};
-        let expectedOutput = new Array(7).fill(" ").join('') + 5;
+        let expectedOutput = new Array(7).fill(" ").join('') + 4;
          expectedOutput += new Array(7).fill(" ").join('') + 5;
          expectedOutput += new Array(7).fill(" ").join('') + 9;
          expectedOutput += " " + "alphabets";
         assert.equal(contentCount("alphabets", fs), expectedOutput); 
+    })
+
+    it('should return every content count 0 and filename for empty file', () => {
+        let expectedOutput = new Array(7).fill(" ").join('') + 0;
+         expectedOutput += new Array(7).fill(" ").join('') + 0;
+         expectedOutput += new Array(7).fill(" ").join('') + 0;
+         expectedOutput += " " + "empty";
+        assert.equal(contentCount("empty", fs), expectedOutput); 
     })
 })
