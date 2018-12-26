38a978f25fc1591ff7e70595d6d4fee48131d246
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-26 14:07:35 +0530

    Handling extra lines and words error.

--- src/lib.js
+++ src/lib.js
@@ -3,35 +3,37 @@ class Contents {
         this.contents = contents;
     }
 
-    getLineCount(){
-        return this.contents.split('\n').length;
+    getLineCount() {
+        return this.contents.split('\n').length - 1;
     }
 
-    getWordCount(){
+    getEachLineWordCount (count, line) {
+        return count + line.split(' ').filter(word => word != "").length
+    }
+
+    getWordCount() {
         const lines = this.contents.trim().split('\n');
-        return lines.reduce((count,line) => {
-            return count + line.split(' ').length 
-        }, 0);
+        return lines.reduce(this.getEachLineWordCount, 0);
     }
 
-    getByteCount(){
+    getByteCount() {
         return this.contents.length;
     }
 }
 
-const formatOutput = function(counts, file) {
-   const { lineCount, wordCount, byteCount } = counts;
-   const noOfLineCountSpaces =  8 - (""+ lineCount).length;
-   const noOfWordCountSpaces =  8 - (""+ wordCount).length;
-   const noOfByteCountSpaces =  8 - (""+ byteCount).length;
-   const lineCountSpaces = new Array(noOfLineCountSpaces).fill(" ").join('');
-   const wordCountSpaces = new Array(noOfWordCountSpaces).fill(" ").join('');
-   const byteCountSpaces = new Array(noOfByteCountSpaces).fill(" ").join('');
-   let result = lineCountSpaces + lineCount + wordCountSpaces + wordCount;
-   return result + byteCountSpaces + byteCount + " " + file;
+const formatOutput = function (counts, file) {
+    const { lineCount, wordCount, byteCount } = counts;
+    const noOfLineCountSpaces = 8 - ("" + lineCount).length;
+    const noOfWordCountSpaces = 8 - ("" + wordCount).length;
+    const noOfByteCountSpaces = 8 - ("" + byteCount).length;
+    const lineCountSpaces = new Array(noOfLineCountSpaces).fill(" ").join('');
+    const wordCountSpaces = new Array(noOfWordCountSpaces).fill(" ").join('');
+    const byteCountSpaces = new Array(noOfByteCountSpaces).fill(" ").join('');
+    let result = lineCountSpaces + lineCount + wordCountSpaces + wordCount;
+    return result + byteCountSpaces + byteCount + " " + file;
 }
 
-const contentCount = function(file, fs) {
+const contentCount = function (file, fs) {
     const fileContents = fs.readFileSync(file, "utf8")
     const contents = new Contents(fileContents, fs);
     const lineCount = contents.getLineCount();
