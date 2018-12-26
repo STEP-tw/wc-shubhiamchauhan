452b30f49be06efdb31a4b09c4eda54f5960fd8a
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-26 18:29:32 +0530

    Added error test for invalid options.

--- test/parseInputTest.js
+++ test/parseInputTest.js
@@ -6,35 +6,44 @@ describe('parse', function () {
         const actualOutput = parse(["alphabets", "numbers"]);
         const exceptedOutput = {
             options: ["lineCount", "wordCount", "byteCount"],
-            files:["alphabets", "numbers"]
-        }
+            files: ["alphabets", "numbers"]
+        };
         assert.deepEqual(actualOutput, exceptedOutput);
     })
 
     it('should return only lineCount option and files in an object for only files list', () => {
-        const actualOutput = parse(["-l","alphabets", "numbers"]);
+        const actualOutput = parse(["-l", "alphabets", "numbers"]);
         const exceptedOutput = {
             options: ["lineCount"],
-            files:["alphabets", "numbers"]
-        }
+            files: ["alphabets", "numbers"]
+        };
         assert.deepEqual(actualOutput, exceptedOutput);
     })
 
     it('should return only wordCount option and files in an object for only files list', () => {
-        const actualOutput = parse(["-w","alphabets", "numbers"]);
+        const actualOutput = parse(["-w", "alphabets", "numbers"]);
         const exceptedOutput = {
             options: ["wordCount"],
-            files:["alphabets", "numbers"]
-        }
+            files: ["alphabets", "numbers"]
+        };
         assert.deepEqual(actualOutput, exceptedOutput);
     })
 
     it('should return only byteCount option and files in an object for only files list', () => {
-        const actualOutput = parse(["-c","alphabets", "numbers"]);
+        const actualOutput = parse(["-c", "alphabets", "numbers"]);
         const exceptedOutput = {
             options: ["byteCount"],
-            files:["alphabets", "numbers"]
-        }
+            files: ["alphabets", "numbers"]
+        };
+        assert.deepEqual(actualOutput, exceptedOutput);
+    })
+
+    it('should return hasError and option error message an object for invalid option', () => {
+        const actualOutput = parse(["-n", "alphabets", "numbers"]);
+        const exceptedOutput = {
+            hasError: true,
+            message: "wc: illegal option -- n\nusage: wc [-clmw] [file ...]"
+        };
         assert.deepEqual(actualOutput, exceptedOutput);
     })
 })
