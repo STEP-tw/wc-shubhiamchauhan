490a115a31437000c2f0a3fcf80838f229682380
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-12-27 11:19:00 +0530

    Now working for multiple files also.

--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,11 @@
+const addSameIndexValues = function (firstList, secondList) {
+    let sum = [];
+    for (let index = 0; index < firstList.length; index++) {
+        sum[index] = firstList[index] + secondList[index];
+    }
+    return sum;
+}
+
 const getLineCount = function (contents) {
     return contents.split('\n').length - 1;
 }
@@ -20,7 +28,7 @@ const justify = (count) => {
     const spaces = new Array(numOfSpaces).fill(" ").join('');
     return spaces + count;
 }
-const formatOutput = function (counts, file) {
+const formatOutput = function ({counts, file}) {
     const justifiedCounts = counts.map(count => justify(count));
     return justifiedCounts.join('') + " " + file;
 }
@@ -33,13 +41,32 @@ const getContentCounts = function (options, fs, file) {
         byteCount: getByteCount
     }
     const counts = options.map(type => counter[type](contents));
-    return formatOutput(counts, file);
+    return { counts, file };
+}
+
+const multipleFileFormatter = function (countsAndFilenames) {
+    const length = countsAndFilenames[0].counts.length;
+    let total = new Array(length).fill(0);
+    const totalCount = countsAndFilenames.reduce((total, { counts }) => {
+        return addSameIndexValues(total, counts);
+    }, total);
+    const totalContents = { counts: totalCount, file: "total" };
+    return countsAndFilenames.concat(totalContents).map(countsAndfile =>
+        formatOutput(countsAndfile)
+    );
+}
+
+const singleFileFormatter = function(countsAndFilenames){
+    return countsAndFilenames.map(data => formatOutput(data));
 }
 
 const contentCount = function (args, fs) {
     const { options, files } = args;
+    let formatter = multipleFileFormatter;
+    if(files.length < 2) formatter = singleFileFormatter;
     const getAllCounts = getContentCounts.bind(null, options, fs);
-    return files.map(file => getAllCounts(file)).join('\n');
+    let result = files.map(file => getAllCounts(file));
+    return formatter(result).join('\n');
 }
 
 module.exports = {
